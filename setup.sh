a084a5cded1204fa4ed47893bc74505a4a4eaceb
Eran Heres <eran.heres@gmail.com>
2019-01-10 15:50:33 +0000

    Change .vimrc only if needed. Add script to install vim-plug

--- setup.sh
+++ setup.sh
@@ -1,1 +1,5 @@
-echo "source vimconfig.sh" >> ~/.vimrc
+if [[ -z $(grep vimrc-config ~/.vimrc) ]]; then
+	echo adding config to .vimrc
+	echo "source $(pwd)/vimrc-config" >> ~/.vimrc
+fi
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim

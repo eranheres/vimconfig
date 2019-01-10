ca77fcb795010257a6ff6d14244c50d113427861
Eran Heres <eran.heres@gmail.com>
2019-01-10 16:56:34 +0000

    v1.0

--- setup.sh
+++ setup.sh
@@ -2,4 +2,3 @@ if [[ -z $(grep vimrc-config ~/.vimrc) ]]; then
 	echo adding config to .vimrc
 	echo "source $(pwd)/vimrc-config" >> ~/.vimrc
 fi
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim

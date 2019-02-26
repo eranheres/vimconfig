279d819fc454179fdcec272cd8c3d1635508f4c3
Eran Heres <eran.heres@gmail.com>
2019-02-26 12:00:59 +0200

    more changes

---
+++ komodo-python/run.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+python -S ~/.vim/komodo-python/pydbgp -d localhost:9000 $1 
--- setup.sh
+++ setup.sh
@@ -3,3 +3,7 @@ if [[ -z $(grep vimrc-config ~/.vimrc) ]]; then
 	echo adding config to .vimrc
 	echo "source $(pwd)/vimrc-config" >> ~/.vimrc
 fi
+vim +PluginInstall +qa
+cp -R komodo-python ~/.vim
+cd ~/.vim/bundle/YouCompleteMe
+python install.py

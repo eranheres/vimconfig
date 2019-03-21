0bfd8cc216de26306d78fdfddb3b53bba0b59eb5
Eran Heres <eran.heres@gmail.com>
2019-03-21 13:44:38 +0000

    neovim setup script, changing plugin manager

--- neovim_setup.sh
+++ neovim_setup.sh
@@ -1,11 +1,11 @@
 #!/bin/sh 
-if [[ ! -f $(grep vimrc-config ~/.vimrc) ]]; then
-	ln -sf $(PWD)/init.vim $(HOME)/.config/nvim/init.vim
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+if [[ ! -d $HOME/.config/nvim/init.vim ]]; then
+	mkdir -p $HOME/.config/nvim
+	ln -sf $pwd/init.vim $HOME/.config/nvim/init.vim
 fi
-if [[ ! -d "~/.vim/bundle/Vundle.vim" ]]; then 
-	git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-fi
-nvim +PluginInstall +qa
-cp -R komodo-python ~/.vim
-cd ~/.vim/bundle/YouCompleteMe
-python install.py
+#nvim +PluginInstall +qa
+#cp -R komodo-python ~/.vim
+#cd ~/.vim/bundle/YouCompleteMe
+#python install.py

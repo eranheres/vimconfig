aa2a4a0cdeb243c1aed156b4501ef0a823eef101
eranh <eran.heres@gmail.com>
2019-03-21 15:11:24 +0200

    adding setup script for noevim

---
+++ neovim_setup.sh
@@ -0,0 +1,11 @@
+#!/bin/sh 
+if [[ ! -f $(grep vimrc-config ~/.vimrc) ]]; then
+	ln -sf $(PWD)/init.vim $(HOME)/.config/nvim/init.vim
+fi
+if [[ ! -d "~/.vim/bundle/Vundle.vim" ]]; then 
+	git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
+nvim +PluginInstall +qa
+cp -R komodo-python ~/.vim
+cd ~/.vim/bundle/YouCompleteMe
+python install.py
